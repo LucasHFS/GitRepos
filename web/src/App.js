258e724ed77a98793bf6abe39e0f9312a7c59191
lucashfs <lucash.silva1612@gmail.com>
2020-07-30 18:57:36 -0300

    First Commit

---
+++ web/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Routes from './Routes';
+import GlobalStyles from './styles/global';
+
+function App() {
+  return (
+    <>
+      <Routes />
+      <GlobalStyles />
+    </>
+  );
+}
+
+export default App;
